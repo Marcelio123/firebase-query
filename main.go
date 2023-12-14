4a4f6138608f54a9f6cbfae4abc28b7815584b5d
Marcelio Darel Liem <marceliodarelliem@yahoo.co.id>
2023-12-14 16:47:33 +0800

    Add grand total, add more \n to make the message prettier

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Get today's date
 	now := time.Now()
 	year, month, day := now.Date()
-	today := fmt.Sprintf("%d-%02d-%02d", year, month, day)
+	today := fmt.Sprintf("%02d-%02d-%d", day, month, year)
 
 	iter := client.Collection("branches").Documents(mainCtx)
 
@@ -55,14 +55,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		msg += fmt.Sprintf("Untuk cabang: %s\n", branch.Name)
-		msg += fmt.Sprintf("Penjualan %s pada tanggal %s telah di jumlahkan di bawah.\n", branch.Name, today)
+		msg += fmt.Sprintf("Penjualan %s pada tanggal %s telah di jumlahkan di bawah.\n\n", branch.Name, today)
 		msg += fmt.Sprintf("Ini dia ringkasannya:\n")
 
 		transaction_summaries_itter := client.Collection("transaction_summaries").
-			Where("date", "==", today).
-			Where("branch_uuid", "==", branch.UUID).
-			Documents(mainCtx)
+		Where("date", "==", today).
+		Where("branch_uuid", "==", branch.UUID).
+		Documents(mainCtx)
 		branch_payment := make(map[string]float64)
+		var total_sales float64
+		
 		// iterate transaction summaries
 		for {
 			snap, err := transaction_summaries_itter.Next()
@@ -70,61 +72,62 @@ func handler(w http.ResponseWriter, r *http.Request) {
 				log.Print(err)
 				break
 			}
-
+			
 			var transaction_summaries model.TransactionSummaries
 			if err := snap.DataTo(&transaction_summaries); err != nil {
 				log.Print("failed parsing transaction summaries")
 				panic(err)
 			}
-
+			
 			for _, payment_group := range transaction_summaries.PaymentTypes {
 				for key, payment_name := range payment_group {
 					total := payment_name.Total
 					branch_payment[key] += total
 				}
 			}
-
+			
 			for key, value := range branch_payment {
-				msg += fmt.Sprintf("%s: %s\n", key, math.FormatCurrency(value))
+				msg += fmt.Sprintf("%s:\n %s\n\n", key, math.FormatCurrency(value))
 			}
+			total_sales = transaction_summaries.TotalSales
 			msg += fmt.Sprintf("Total diskon: %s\n", math.FormatCurrency(transaction_summaries.TotalDiscount))
-			msg += fmt.Sprintf("Total penjualan: %s\n", math.FormatCurrency(transaction_summaries.TotalSales))
+			msg += fmt.Sprintf("Total transaksi: %s\n", math.FormatCurrency(transaction_summaries.TotalSales))
+			
 		}
-
 		// Laporan kas
-		msg += fmt.Sprintf("Laporan Kas\n")
+		msg += fmt.Sprintf("\nLaporan Kas\n")
+		var total_expanse float64
 		employee_shifts_iter := client.Collection("employee_shifts").
-		    Where("branch_uuid", "==", branch.UUID).
-		    Where("date", "==", today).
-		    Documents(mainCtx)
+			Where("branch_uuid", "==", branch.UUID).
+			Where("date", "==", today).
+			Documents(mainCtx)
+		
 		for {
-		    snap, err := employee_shifts_iter.Next()
-		    if err != nil {
-		        fmt.Println(err)
-		        break
-		    }
-
-		    var employee_shifts model.EmployeeShifts
-		    if err := snap.DataTo(&employee_shifts); err != nil {
-		        fmt.Println("failed parsing employee shifts")
-		        panic(err)
-		    }
-		    msg += fmt.Sprintf("Kas awal: %s", math.FormatCurrency(employee_shifts.StartCash))
-		    var total_expanse float64
-		    for _, value := range employee_shifts.CashEntries {
-		        msg += fmt.Sprintf("- %s %s", value.Description, math.FormatCurrency(value.Value))
-		        total_expanse += value.Value
-		    }
-		    if employee_shifts.EndCash != nil {
-		        // EndCash exists in Firestore, and its value is not nil
-		        // Perform computation
-		        total_expanse = employee_shifts.StartCash + *employee_shifts.EndCash
-		        msg += fmt.Sprintf("Total Kas: %s", math.FormatCurrency(total_expanse))
-		    } else {
-		        // EndCash is absent in Firestore (or its value is nil)
-		        msg += fmt.Sprintf("EndCash is absent or nil, cannot compute total cash\n")
-		    }
-
+			snap, err := employee_shifts_iter.Next()
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+	
+			var employee_shifts model.EmployeeShifts
+			if err := snap.DataTo(&employee_shifts); err != nil {
+				fmt.Println("failed parsing employee shifts")
+				panic(err)
+			}
+			msg += fmt.Sprintf("Kas awal: %s\n", math.FormatCurrency(employee_shifts.StartCash))
+			for _, value := range employee_shifts.CashEntries {
+				msg += fmt.Sprintf("- %s %s\n", value.Description, math.FormatCurrency(value.Value))
+				total_expanse += value.Value
+			}
+			if employee_shifts.EndCash != nil {
+				// EndCash exists in Firestore, and its value is not nil
+				// Perform computation
+				total_expanse = employee_shifts.StartCash + *employee_shifts.EndCash
+				msg += fmt.Sprintf("\nTotal Kas: %s\n", math.FormatCurrency(total_expanse))
+			} else {
+				// EndCash is absent in Firestore (or its value is nil)
+				msg += fmt.Sprintf("EndCash is absent or nil, cannot compute total cash\n")
+			}
 		}
 
 		var tertampung float64
@@ -133,7 +136,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			Where("deleted_at", "==", nil).
 			Documents(mainCtx)
 
-		msg += fmt.Sprintf("Pesanan tertampung: \n")
+		msg += fmt.Sprintf("\nPesanan tertampung: \n")
 		for {
 			snap, err := activeOrderGroupIter.Next()
 			if err != nil {
@@ -176,11 +179,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 				}
 				price -= total_discount
 
-				msg += fmt.Sprintf("%s %s\n", orderGroup.UUID, math.FormatCurrency(price))
+				msg += fmt.Sprintf("-%s: %s\n", orderGroup.UUID, math.FormatCurrency(price))
 				tertampung += price
 			}
 		}
-		msg += fmt.Sprintf("Total Pesanan Tertampung\n %s", math.FormatCurrency(tertampung))
+		msg += fmt.Sprintf("\nTotal Pesanan Tertampung %s\n\n", math.FormatCurrency(tertampung))
+		grand_total := total_sales - total_expanse
+		msg += fmt.Sprintf("Grand total: %s\n", math.FormatCurrency(grand_total))
+		msg += fmt.Sprintf("Terima Kasih telah menggunakan Lucy <3\n")
 
 		resp, err := whatsapp.SendMessage(mainCtx, wac, fmt.Sprintf("%s%s", branch.Whatsapp.CountryNumber, branch.Whatsapp.Number), msg)
 		if err != nil {
